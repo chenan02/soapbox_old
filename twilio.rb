4974542995972237e20a8039023561074fa3a53c
Andrew Chen <chenan02@gmail.com>
2016-04-02 11:18:27 -0400

    twilio api stuff

---
+++ twilio.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'twilio-ruby'
+ 
+def send_message(body)
+	account_sid = "AC892d1ad25d2cd70dd0880beb725a1bf4"
+	auth_token = "4b4820bbab194b3cb12ae7e4352b1d10"
+	client = Twilio::REST::Client.new account_sid, auth_token
+	 
+	from = "+12488262836" # Your Twilio number
+	 
+	friends = {
+	"2483809188" => "Andrew Chen"
+	}
+	friends.each do |key, value|
+	  client.account.messages.create(
+	    :from => from,
+	    :to => key,
+	    :body => body
+	  )
+	  puts "Sent message to #{value}"
+	end
+end
